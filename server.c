f30a19ea96ea23b0bbf481ada5da6cd73232bf9a
dfurneau <42@dfurneaux.com>
2022-12-13 21:37:53 +0000

    Update server.c format

--- server.c
+++ server.c
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #define PORT 1337
 
+// Buffer Copy Code
 int buff_copy(char *str) {
 	char copy[1024];
 
@@ -29,10 +30,8 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET,
-                   SO_REUSEADDR | SO_REUSEPORT, &opt,
-                   sizeof(opt))) {
+    // Forcefully attaching socket to the port 1337
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
@@ -40,31 +39,27 @@ int main(void)
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr*)&address,
-             sizeof(address))
-        < 0) {
+    // Forcefully attaching socket to the port 1337
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
+	// Start Listening on the socket
     if (listen(server_fd, 3) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    if ((new_socket
-         = accept(server_fd, (struct sockaddr*)&address,
-                  (socklen_t*)&addrlen))
-        < 0) {
+	// Setup socket accept to receive data
+    if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
         perror("accept");
         exit(EXIT_FAILURE);
     }
-
+	// Our While Loop that reads 4096 Bytes, copies into our buffer and sends it back to the client
     while (valread = read(new_socket, buffer, 4096)) {
 		buff_copy(buffer);
     	printf("buffer: %s bytes: %d\n", buffer, valread);
     	send(new_socket, buffer, strlen(buffer), 0);
 	}
-
     // closing the connected socket
     close(new_socket);
     // closing the listening socket
